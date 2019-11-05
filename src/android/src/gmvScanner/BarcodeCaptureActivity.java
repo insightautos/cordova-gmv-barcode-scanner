33d49aa24e0dfbae403240460b0dcf58e427417d
Forrest Middleton <forrest@dealr.cloud>
2019-11-05 07:55:19 -0700

    Screen orientation lock.

--- src/android/src/gmvScanner/BarcodeCaptureActivity.java
+++ src/android/src/gmvScanner/BarcodeCaptureActivity.java
@@ -21,6 +21,7 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.pm.ActivityInfo;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -112,6 +113,7 @@ public final class BarcodeCaptureActivity extends AppCompatActivity implements B
 
         setContentView(getResources().getIdentifier("barcode_capture", "layout", getPackageName()));
 
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         mPreview = (CameraSourcePreview) findViewById(getResources().getIdentifier("preview", "id", getPackageName()));
         mPreview.ViewFinderWidth = ViewFinderWidth;
@@ -287,6 +289,7 @@ public final class BarcodeCaptureActivity extends AppCompatActivity implements B
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
         if (mPreview != null) {
             mPreview.release();
         }
